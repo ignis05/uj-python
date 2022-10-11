6044a4f391f2560e2ecf2d9dd37c976c02880e12
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-10-11 14:05:00 +0200

    replace l02 with ipynb notebook

--- l02/2.11.py
+++
@@ -1,6 +0,0 @@
-# Podać sposób wyświetlania napisu word tak, aby jego znaki były rozdzielone znakiem podkreślenia.
-
-word = 'word'
-
-result = '_'.join(list(word))
-print(result)
--- l02/2.16.py
+++
@@ -1,8 +0,0 @@
-# W tekście znajdującym się w zmiennej line zamienić ciąg znaków "GvR" na "Guido van Rossum"
-
-line = "Ex officia sint minim reprehenderit GvR proident Lorem adipisicing ex. Et commodo excepteur eu Lorem non non. GvR Non et proident velit tempor sunt labore ullamco Lorem aliquip magna."
-
-newLine = line.replace('GvR','Guido van Rossum')
-
-print(line)
-print(newLine)
