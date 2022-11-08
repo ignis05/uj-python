43377980e16beb05f0e0bc28272561f66d9259b6
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-11-08 13:00:35 +0100

    l07: done

--- l07/7.3.py
+++ l07/7.3.py
@@ -7,6 +7,8 @@ class Rectangle:
 
     def __init__(self, x1, y1, x2, y2):
         # Chcemy, aby x1 < x2, y1 < y2.
+        if x1 >= x2 or y1 >= y2:
+            raise ValueError("Point 1 must have lower coordinates than Point 2")
         self.pt1 = Point(x1, y1)
         self.pt2 = Point(x2, y2)
 
@@ -36,11 +38,34 @@ class Rectangle:
         self.pt1 += pt3
         self.pt2 += pt3
 
-    def intersection(self, other): pass  # część wspólna prostokątów
+    def intersection(self, other):  # część wspólna prostokątów
+        if not isinstance(other, Rectangle):
+            raise ValueError("Other is not a rectangle")
+        h_p1_X = max(self.pt1.x, other.pt1.x)
+        h_p1_Y = max(self.pt1.y, other.pt1.y)
+        l_p2_X = min(self.pt2.x, other.pt2.x)
+        l_p2_Y = min(self.pt2.y, other.pt2.y)
+        if h_p1_X >= l_p2_X or h_p1_Y >= l_p2_Y:
+            raise ValueError("Rectangles have no intersection")
+        return Rectangle(h_p1_X, h_p1_Y, l_p2_X, l_p2_Y)
 
-    def cover(self, other): pass    # prostąkąt nakrywający oba
+    def cover(self, other):    # prostąkąt nakrywający oba
+        if not isinstance(other, Rectangle):
+            raise ValueError("Other is not a rectangle")
+        lowestX = min(self.pt1.x, other.pt1.x)
+        lowestY = min(self.pt1.y, other.pt1.y)
+        heighestX = max(self.pt2.x, other.pt2.x)
+        heighestY = max(self.pt2.y, other.pt2.y)
+        return Rectangle(lowestX, lowestY, heighestX, heighestY)
 
-    def make4(self): pass           # zwraca krotkę czterech mniejszych
+    def make4(self):           # zwraca krotkę czterech mniejszych
+        center = self.center()
+        return (
+            Rectangle(self.pt1.x, center.y, center.x, self.pt2.y),  # A
+            Rectangle(center.x, center.y, self.pt2.x, self.pt2.y),  # B
+            Rectangle(center.x, self.pt1.y, self.pt2.x, center.y),  # C
+            Rectangle(self.pt1.x, self.pt1.y, center.x, center.y),  # D
+        )
 # A-------B   po podziale  A---+---B
 # |       |                |   |   |
 # |       |                +---+---+
@@ -83,6 +108,36 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1, Rectangle(0, 0, 2, 2))
         r1.move(1, -1)
         self.assertEqual(r1, Rectangle(1, -1, 3, 1))
+
+    def test_cover(self):
+        r1 = Rectangle(1, 1, 3, 3)
+        r2 = Rectangle(2, 2, 4, 4)
+        self.assertEqual(r1.cover(r1), r1)
+        self.assertEqual(r2.cover(r2), r2)
+        self.assertEqual(r1.cover(r2), r2.cover(r1))
+        r3 = r1.cover(r2)
+        self.assertEqual(r3.cover(r1), r3)
+        self.assertEqual(r3.cover(r2), r3)
+        self.assertEqual(r3, Rectangle(1, 1, 4, 4))
+
+    def test_intersection(self):
+        r1 = Rectangle(1, 1, 3, 3)
+        r2 = Rectangle(2, 2, 4, 4)
+        self.assertEqual(r1.intersection(r2), r2.intersection(r1))
+        r3 = r1.intersection(r2)
+        self.assertEqual(r3, Rectangle(2, 2, 3, 3))
+        self.assertEqual(r3, r3.intersection(r1))
+        self.assertEqual(r3, r3.intersection(r2))
+
+    def test_make4(self):
+        r1, r2, r3, r4 = Rectangle(0, 0, 4, 4).make4()
+        self.assertEqual(r1, Rectangle(0, 2, 2, 4))
+        self.assertEqual(r2, Rectangle(2, 2, 4, 4))
+        self.assertEqual(r3, Rectangle(2, 0, 4, 2))
+        self.assertEqual(r4, Rectangle(0, 0, 2, 2))
+        self.assertEqual(r1.area(), r2.area())
+        self.assertEqual(r2.area(), r3.area())
+        self.assertEqual(r3.area(), r4.area())
 
 
 if __name__ == '__main__':
