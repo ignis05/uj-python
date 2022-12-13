f85360449c001a9ca30e6330a1171a9953857b45
ignis05 <48099798+ignis05@users.noreply.github.com>
2022-12-13 12:44:44 +0100

    10.4: more tests

--- l10/test_queue.py
+++ l10/test_queue.py
@@ -5,22 +5,37 @@ import pytest
 def test_queue():
     q = Queue(5)
 
+    assert q.is_empty() == True
+    assert q.is_full() == False
+
     with pytest.raises(ValueError):
         q.get()
 
     q.put(1)
     q.put(2)
     q.put(3)
+    
+    assert q.is_empty() == False
+    assert q.is_full() == False
+    
     q.put(4)
     q.put(5)
+
+    assert q.is_empty() == False
+    assert q.is_full() == True
 
     with pytest.raises(ValueError):
         q.put(6)
 
     assert q.get() == 1
     assert q.get() == 2
+    
+    assert q.is_full() == False
+    
     q.put(6)
     q.put(7)
+    
+    assert q.is_full() == True
 
     with pytest.raises(ValueError):
         q.put(6)
